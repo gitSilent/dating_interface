d0e57f2b1c51e43712a8f68c2223d7b4a4be3ba1
Dmitry <dmitrym2019@mail.ru>
2023-05-01 19:43:04 +0300

    Инициализация. Пустые компоненты гланых страниц

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import AuthorizationPage from './AuthorizationPage/AuthorizationPage';
+import RegistrationPage from './RegistrationPage/RegistrationPage';
+import MainPage from './MainPage/MainPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/authorization",
+    element: <AuthorizationPage />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/registration",
+    element: <RegistrationPage />,
+    errorElement: <ErrorPage />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
