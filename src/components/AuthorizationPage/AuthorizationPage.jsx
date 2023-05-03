6088f6c34007b1bfc59ba14646dd6cfb46ff05c6
Dmitry <dmitrym2019@mail.ru>
2023-05-03 21:15:46 +0300

    Не полностью доделана авторизация и регистрация с валидацией

--- src/components/AuthorizationPage/AuthorizationPage.jsx
+++ src/components/AuthorizationPage/AuthorizationPage.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './AuthorizationPage.module.css'
 import { Box } from '@mui/material'
 import {TextField, Button, Typography} from '@mui/material'
+import { Link } from 'react-router-dom'
+import sha256 from 'sha256'
 
 export default function AuthorizationPage() {
+  const[login, setLogin] = useState();
+  const[password, setPassword] = useState();
+
+  async function authorizate(){
+    console.log(login + " " + password);
+
+    let response = await fetch(`http://localhost:3050/getUserByPhone/${login}`)
+    let data = await response.json()
+    console.log(data)
+    if(login == data[0].phone_number && data[0].password == sha256(password)){
+      console.log('authorized');
+    }else{
+      console.log('not authorized');
+    }
+  }
   return (
     <div className={styles.authorizationPage}>
       
-      <div className='form'>
+      <form className='form' onSubmit={(e)=>{
+        e.preventDefault()
+        authorizate();
+        }}>
         <Box
           display='flex'
           justifyContent='center'
@@ -20,12 +40,12 @@ export default function AuthorizationPage() {
           boxShadow={'5px 5px 10px #ccc'}
         >
         <h2>Авторизация</h2>
-        <TextField fullWidth={true} margin='normal' label="Номер телефона" variant="outlined" placeholder='Введите номер телефона'/>
-        <TextField fullWidth={true} margin='normal' label="Пароль" variant="outlined" placeholder='Введите пароль'/>
-        <Button sx={{fontFamily:'Popins', fontSize:'22px', fontStyle:'normal', backgroundColor:'black', padding:'5px 204px 5px 204px', marginTop:'10px', maxWidth:'485px'}} margin='normal' variant="contained">Войти</Button>
-        <Typography variant="body1" sx={{fontFamily: 'Popins', marginTop:'15px' }}> Нет аккаунта? <span className="linkReg"> Регистрация</span></Typography>
+        <TextField onChange={(e)=>{setLogin(e.target.value)}} fullWidth={true} margin='normal' label="Номер телефона" variant="outlined" placeholder='Введите номер телефона'/>
+        <TextField onChange={(e)=>{setPassword(e.target.value)}} fullWidth={true} margin='normal' label="Пароль" variant="outlined" placeholder='Введите пароль'/>
+        <Button type='submit' sx={{fontFamily:'InterSemiBold',textTransform: 'none', fontSize:'22px', fontStyle:'normal', backgroundColor:'black', padding:'5px 204px 5px 204px', marginTop:'10px', maxWidth:'485px'}} margin='normal' variant="contained">Войти</Button>
+        <Typography variant="body1" sx={{fontFamily: 'InterSemiBold', marginTop:'15px' }}> Нет аккаунта? <Link to={'/registration'} className={styles.linkReg}> Регистрация</Link></Typography>
         </Box>
-      </div>
+      </form>
     </div>
   )
 }
--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,16 +1,84 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './RegistrationPage.module.css'
 import { Box } from '@mui/material'
-import {TextField, Button, Typography} from '@mui/material'
+import {TextField, Button, Typography, Select, MenuItem, InputLabel} from '@mui/material'
 import sha256 from 'sha256'
+import { Link } from 'react-router-dom'
+
 export default function RegistrationPage() {
-  
-  function getHash(){
-    console.log();
+
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("Мужской");
+  const [city, setCity] = useState("");
+  const [birthDate, setBirthDate] = useState();
+  const [about, setAbout] = useState("");
+  const [height, setHeight] = useState();
+  const [weight, setWeight] = useState();
+
+  const [cities, setCities] = useState([]);
+
+  async function isAlreadyRegistred(){
+    // console.log(login + " " + password);
+    try{
+      let response = await fetch(`http://localhost:3050/getUserByPhone/${login}`)
+      let data = await response.json()
+      console.log(data)
+      return data.length > 0 ? true : false 
+    }catch(er){
+      console.log(er);
+      return false
+    }
+
   }
+
+  async function registrate(){
+   if (await isAlreadyRegistred()){
+    console.log('already registred')
+    alert("Пользователь с таким номером уже зарегистрирован")
+    return
+   }else{
+    console.log('no');
+    console.log(sha256(password));
+
+    let response = await fetch('http://localhost:3050/registrate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({
+        login,
+        password:sha256(password),
+        name,
+        gender,
+        city,
+        birthDate,
+        about,
+        height,
+        weight
+      })
+    });
+   }
+  }
+
+
+  useEffect(()=>{
+    fetch('http://localhost:3050/getCities')
+    .then(response => response.json())
+    .then((data)=>{
+      console.log(data.result);
+      setCities(data.result)
+    })
+  },[])
+
+
   return (
     <div className={styles.registrationPage}>
-      <div className='form'>
+      <form className='form' onSubmit={(e)=>{
+        e.preventDefault()
+        registrate();
+        }}>
         <Box
           display='flex'
           justifyContent='center'
@@ -23,12 +91,48 @@ export default function RegistrationPage() {
           boxShadow={'5px 5px 10px #ccc'}
         >
         <h2>Регистрация</h2>
-        <TextField fullWidth={true} margin='normal' label="Номер телефона" variant="outlined" placeholder='Введите номер телефона...'/>
-        <TextField fullWidth={true} margin='normal' label="Пароль" variant="outlined" placeholder='Придумайте пароль...'/>
-        <Button sx={{fontFamily:'Popins', fontSize:'22px', fontStyle:'normal', backgroundColor:'black', padding:'5px 204px 5px 204px', marginTop:'10px', maxWidth:'485px'}} margin='normal' variant="contained">Зарегистрироваться</Button>
-        <Typography variant="body1" sx={{fontFamily: 'Popins', marginTop:'15px' }}> Уже есть аккаунт? <span className="linkAuth">Авторизация</span></Typography>
+        <TextField required maxlength="11" onChange={(e)=>{setLogin(e.target.value)}} type='phone' fullWidth={true} margin='normal' label="Номер телефона" variant="outlined" placeholder='Введите номер телефона...'/>
+        <TextField required onChange={(e)=>{setPassword(e.target.value)}} fullWidth={true} type='password' margin='normal' label="Пароль" variant="outlined" placeholder='Придумайте пароль...'/>
+        <TextField required onChange={(e)=>{setName(e.target.value)}} fullWidth={true} margin='normal' label="Как вас зовут?" variant="outlined" placeholder='Придумайте пароль...'/>
+        {/* <TextField onChange={(e)=>{setGender(e.target.value)}} fullWidth={true} margin='normal' label="Пароль" variant="outlined" placeholder='Придумайте пароль...'/>
+        <TextField onChange={(e)=>{setCity(e.target.value)}} fullWidth={true} margin='normal' label="Пароль" variant="outlined" placeholder='Придумайте пароль...'/> */}
+        <InputLabel>Гендер</InputLabel>
+        <Select 
+        native
+        required
+        fullWidth={true}
+          defaultValue={"Мужской"}
+          onChange={(event)=>{setGender(event.target.value)}}>
+          <option value={"Мужской"}>Мужской</option>
+          <option value={"Женский"}>Женский</option>
+          <option value={"Би"}>Би</option>
+          <option value={"MtF"}>MtF</option>
+          <option value={"FtM"}>FtM</option>
+          <option value={"Другой"}>Другой</option>
+        </Select>
+
+        <InputLabel>Город</InputLabel>
+        <Select 
+        native
+        required
+        fullWidth={true}
+          defaultValue={0} // доделать валидацию с селектами и авторизацию
+          onChange={(event)=>{setCity(event.target.value)}}>
+            {
+              cities.map((city,index)=>(
+                <option value={city.city_name}>{city.city_name}</option>
+              ))
+            }
+          
+        </Select>
+        <TextField required onChange={(e)=>{setBirthDate(e.target.value)}} type='date' fullWidth={true} margin='normal' variant="outlined" placeholder='Придумайте пароль...'/>
+        <TextField onChange={(e)=>{setAbout(e.target.value)}} fullWidth={true} margin='normal' label="Кратко о себе" variant="outlined" placeholder='Придумайте пароль...'/>
+        <TextField required type='number' InputProps={{ inputProps: { min: 135, max: 230 } }} onChange={(e)=>{setHeight(e.target.value)}} fullWidth={true} margin='normal' label="Ваш рост" variant="outlined" placeholder='Придумайте пароль...'/>
+        <TextField required type='number' InputProps={{ inputProps: { min: 35, max: 250 } }} onChange={(e)=>{setWeight(e.target.value)}} fullWidth={true} margin='normal' label="Ваш вес" variant="outlined" placeholder='Придумайте пароль...'/>
+        <Button type='submit' sx={{fontFamily:'InterSemiBold',textTransform: 'none', fontSize:'22px', fontStyle:'normal', backgroundColor:'black', padding:'5px 204px 5px 204px', marginTop:'10px', maxWidth:'485px'}} margin='normal' variant="contained">Зарегистрироваться</Button>
+        <Typography variant="body1" sx={{fontFamily: 'InterSemiBold',textTransform: 'none', marginTop:'15px' }}> Уже есть аккаунт? <Link to={'/authorization'} className={styles.linkAuth}>Авторизация</Link></Typography>
         </Box>
-      </div>
+      </form>
     </div>
   )
 }
